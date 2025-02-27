3fb5c52eb43792da42a82f221b554d20a0e0abe1
expo25 <dsidari25@gmail.com>
2025-02-27 14:35:17 -0700

    config: :wrench: set retry mechanism & add env variable

--- cypress.config.js
+++ cypress.config.js
@@ -2,6 +2,7 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
+    // Using cy.visit('/') will bring us to this url.
     baseUrl: 'https://automationexercise.com',
     // Set to standard HD resolution. I think this will avoid the edge pop-up not taking up the entire screen.
     viewportHeight: 1280,
@@ -22,4 +23,16 @@ module.exports = defineConfig({
       });
     },
   },
+  retries: {
+    /*************************************************************************************\
+    | When we run in headless, any failed test will be retried up to 2 times (3 in total) |
+    | When we run tests in interactive mode, failures will not be auto-retried            |
+    \*************************************************************************************/
+    runMode: 2,
+    openMode: 0
+  },
+  env: {
+    // We don't neccesarily need this cause we already set the baseUrl. But it can useful if we want to reference the full url from our code for reasons other than navigation.
+    url: 'https://automationexercise.com'
+  }
 });
