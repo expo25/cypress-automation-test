9c8e4e854122abe0c5d4c940c560992183ed0044
expo25 <dsidari25@gmail.com>
2025-02-27 14:26:04 -0700

    config: :wrench: update e2e object within config set

--- cypress.config.js
+++ cypress.config.js
@@ -2,6 +2,15 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
+    baseUrl: 'https://automationexercise.com',
+    // Set to standard HD resolution. I think this will avoid the edge pop-up not taking up the entire screen.
+    viewportHeight: 1280,
+    viewportHeight: 720,
+    defaultBrowser: edge,
+    defaultCommandTimeout: 10000,
+    pageLoadTimeout: 60000,
+    screenshotOnRunFailure: true,
+    chromeWebSecurity: false,
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
