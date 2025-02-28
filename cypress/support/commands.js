70c9f546bf8d9270d3ef18339d623d2747396156
expo25 <dsidari25@gmail.com>
2025-02-27 20:05:03 -0700

    clean up code: :wastebasket:

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -7,26 +7,9 @@
 // commands please read more here:
 // https://on.cypress.io/custom-commands
 // ***********************************************
-//
-//
-// -- This is a parent command --
-// Cypress.Commands.add('login', (email, password) => { ... })
-//
-//
-// -- This is a child command --
-// Cypress.Commands.add('drag', { prevSubject: 'element'}, (subject, options) => { ... })
-//
-//
-// -- This is a dual command --
-// Cypress.Commands.add('dismiss', { prevSubject: 'optional'}, (subject, options) => { ... })
-//
-//
-// -- This will overwrite an existing command --
-// Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
 
-// Helper commands used to fill out the signup form(s)
+// Helper command to fill out the signup form
 Cypress.Commands.add('fillSignupForm', (name, email) => {
-    // Here we use attribute selectors.
     cy.get('input[data-qa="signup-name"]').type(name);
     cy.get('input[data-qa="signup-email"]').type(email);
     cy.get('button[data-qa="signup-button"]').click();
@@ -34,20 +17,15 @@ Cypress.Commands.add('fillSignupForm', (name, email) => {
 
 // Helper command to fill in account details
 Cypress.Commands.add('fillAccountDetails', (userData) => {
-    // Here we utilize ID selectors.
     cy.get('#id_gender1').check();
-    // Password
     cy.get('input[data-qa="password"]').type(Cypress.env('TEST_PASSWORD'));
-    // Date of birth
     cy.get('select[data-qa="days"]').select(userData.day);
     cy.get('select[data-qa="months"]').select(userData.month);
     cy.get('select[data-qa="years"]').select(userData.year);
-    /***************************************************************************************************************\
-    | Below should cover all address locators.                                                                      |                  
-    | Here we utilize data-attributes.                                                                              |
-    | This is the method I prefer for testing cause these are most stable & will still work even after UI changes.  |
-    | They also clearly state their purpose.                                                                        |
-    \***************************************************************************************************************/
+    /*
+     * Below covers all address locators.
+     * Uses data-attributes for stability and clarity, as they persist through UI changes.
+     */
     cy.get('input[data-qa="first_name"]').type(userData.firstName);
     cy.get('input[data-qa="last_name"]').type(userData.lastName);
     cy.get('input[data-qa="company"]').type(userData.company);
@@ -58,23 +36,24 @@ Cypress.Commands.add('fillAccountDetails', (userData) => {
     cy.get('input[data-qa="city"]').type(userData.city);
     cy.get('input[data-qa="zipcode"]').type(userData.zipcode);
     cy.get('input[data-qa="mobile_number"]').type(Cypress.env('TEST_MOBILE_NUMBER'));
-    // Submit the actual form
     cy.get('button[data-qa="create-account"]').click();
-    // Commands for logging in
-    Cypress.Commands.add('login', (email, password) => {
-        cy.get('a[href="/login"]').click();
-        cy.contains('Login to your account').should('be.visible');
-        cy.get('input[data-qa="login-email"]').type(email);
-        cy.get('input[data-qa="login-password"]').type(password);
-        cy.get('button[data-qa="login-button"]').click();
-        cy.contains('Logged in as').should('be.visible');
-    });
-    // Commands for payment details
-    Cypress.Commands.add('fillPaymentDetails', (paymentData) => {
-        cy.get('input[data-qa="name-on-card"]').type(paymentData.cardName);
-        cy.get('input[data-qa="card-number"]').type(paymentData.cardNumber);
-        cy.get('input[data-qa="cvc"]').type(paymentData.cvc);
-        cy.get('input[data-qa="expiry-month"]').type(paymentData.expiryMonth);
-        cy.get('input[data-qa="expiry-year"]').type(paymentData.expiryYear);
-    });
-})
+});
+
+// Login
+Cypress.Commands.add('login', (email, password) => {
+    cy.get('a[href="/login"]').click();
+    cy.contains('Login to your account').should('be.visible');
+    cy.get('input[data-qa="login-email"]').type(email);
+    cy.get('input[data-qa="login-password"]').type(password);
+    cy.get('button[data-qa="login-button"]').click();
+    cy.contains('Logged in as').should('be.visible');
+});
+
+// Obtain payment details
+Cypress.Commands.add('fillPaymentDetails', (paymentData) => {
+    cy.get('input[data-qa="name-on-card"]').type(paymentData.cardName);
+    cy.get('input[data-qa="card-number"]').type(paymentData.cardNumber);
+    cy.get('input[data-qa="cvc"]').type(paymentData.cvc);
+    cy.get('input[data-qa="expiry-month"]').type(paymentData.expiryMonth);
+    cy.get('input[data-qa="expiry-year"]').type(paymentData.expiryYear);
+});
