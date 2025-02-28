e569fbffa25b61c8ee852202b8b421bbbae5665e
expo25 <dsidari25@gmail.com>
2025-02-27 20:20:43 -0700

    fx: :bug: remove registered-user reference from beforeEach hook & move into it block.

--- cypress/e2e/checkout.cy.js
+++ cypress/e2e/checkout.cy.js
@@ -1,56 +1,49 @@
 describe('Checkout Process', () => {
     beforeEach(() => {
-        // Load user data and registered credentials
+        // Load fixture data
         cy.fixture('user-data.json').then(function (userData) {
             this.userData = userData;
         });
-        cy.fixture('registered-user.json').then(function (registeredUser) {
-            this.registeredUser = registeredUser;
-        });
         cy.visit('/');
-
-        // Login before checkout
-        cy.login(this.registeredUser.email, this.registeredUser.password);
-
-        // Add an item to cart (simplified for this test)
-        cy.get('a[href="/products"]').click();
-        cy.get('.productinfo').first().find('a[data-product-id]').click();
-        cy.get('.modal-footer').contains('Continue Shopping').click();
     });
+    it('Verify user can complete the checkout process', function () {
+        // Load fixture data & navigate to cart.
+        cy.fixture('registered-user.json').then((registeredUser) => {
+            cy.login(registeredUser.email, registeredUser.password);
+            cy.get('a[href="/products"]').click();
+            cy.get('.productinfo').first().find('a[data-product-id]').click();
+            cy.get('.modal-footer').contains('Continue Shopping').click();
 
-    it('Completes the checkout process and verifies order success', function () {
-        // Navigate to Cart
-        cy.get('a[href="/view_cart"]').first().click();
-        cy.url().should('include', '/view_cart');
-        cy.contains('Shopping Cart').should('be.visible');
+            cy.get('a[href="/view_cart"]').first().click();
+            cy.url().should('include', '/view_cart');
+            cy.contains('Shopping Cart').should('be.visible');
 
-        // Proceed to checkout
-        cy.get('a[href="/login?checkout"]').contains('Proceed To Checkout').click();
+            // Proceed to checkout
+            cy.get('a[href="/login?checkout"]').contains('Proceed To Checkout').click();
 
-        // Verify delivery address matches user data
-        cy.get('.address_delivery').within(() => {
-            cy.contains(this.userData.firstName + ' ' + this.userData.lastName).should('be.visible');
-            cy.contains(this.userData.address1).should('be.visible');
-            cy.contains(this.userData.city).should('be.visible');
+            // Verify delivery address matches user data
+            cy.get('.address_delivery').within(() => {
+                cy.contains(this.userData.firstName + ' ' + this.userData.lastName).should('be.visible');
+                cy.contains(this.userData.address1).should('be.visible');
+                cy.contains(this.userData.city).should('be.visible');
+            });
+
+            // This should be coming from a fixture file, I just did not have time to add it.
+            cy.fillPaymentDetails({
+                cardName: 'Test User',
+                cardNumber: '4111111111111111',
+                cvc: '123',
+                expiryMonth: '12',
+                expiryYear: '2025',
+            });
+
+            // Place an order
+            cy.get('button[data-qa="pay-button"]').click();
+            // Verify success message then continue to homePage
+            cy.contains('Order Placed!').should('be.visible');
+            cy.contains('Congratulations! Your order has been placed successfully!').should('be.visible');
+            cy.get('a[data-qa="continue-button"]').click();
+            cy.url().should('eq', Cypress.config('baseUrl'));
         });
-
-        cy.fillPaymentDetails({
-            cardName: 'Test User',
-            cardNumber: '4111111111111111',
-            cvc: '123',
-            expiryMonth: '12',
-            expiryYear: '2025',
-        });
-
-        // Place order
-        cy.get('button[data-qa="pay-button"]').click();
-
-        // Verify success message
-        cy.contains('Order Placed!').should('be.visible');
-        cy.contains('Congratulations! Your order has been placed successfully!').should('be.visible');
-
-        // Continue to homepage
-        cy.get('a[data-qa="continue-button"]').click();
-        cy.url().should('eq', Cypress.config('baseUrl'));
     });
 });
