6547a5892588593c95a7fda610dceb56f035ab78
expo25 <dsidari25@gmail.com>
2025-02-27 19:53:21 -0700

    fx: :bug: remove registered-user reference from beforeEach hook & move into it block.

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -1,37 +1,29 @@
 describe('User Login and Logout', () => {
     beforeEach(() => {
-        // Load fixture data using the specified method
+        // Load fixture data
         cy.fixture('user-data.json').then(function (userData) {
             this.userData = userData;
-            // Retrieve credentials
             this.userData.password = Cypress.env('TEST_PASSWORD');
         });
-        // Load registered user credentials from registered-user.json
-        cy.fixture('registered-user.json').then(function (registeredUser) {
-            this.registeredUser = registeredUser;
-        });
-
         cy.visit('/');
     });
-});
 
-it('Verify Login/Logout functionality', function () {
-    // Navigate to login page
-    cy.get('a[href="/login"]').click();
-    cy.contains('Login to your account').should('be.visible');
+    it('Verify Login/Logout functionality', function () {
+        // Load registered-user.json directly in the test
+        cy.fixture('registered-user.json').then((registeredUser) => {
+            cy.get('a[href="/login"]').click();
+            cy.contains('Login to your account').should('be.visible');
 
-    // custom login command from dynamically generated registered user file
-    cy.login(this.registeredUser.email, this.registeredUser.password);
+            cy.login(registeredUser.email, registeredUser.password);
 
-    // Verify successful login and redirection to dashboard
-    cy.contains(`Logged in as ${this.userData.name}`).should('be.visible');
-    cy.url().should('eq', Cypress.config('baseUrl'));
-    
-    // logout
-    cy.get('a[href="/logout"]').click();
+            cy.contains(`Logged in as ${this.userData.name}`).should('be.visible');
+            cy.url().should('eq', Cypress.config('baseUrl'));
 
-    // Verify session termination and redirection to login page
-    cy.url().should('include', '/login');
-    cy.contains('Login to your account').should('be.visible');
-    cy.contains('Logged in as').should('not.exist');
+            cy.get('a[href="/logout"]').click();
+
+            cy.url().should('include', '/login');
+            cy.contains('Login to your account').should('be.visible');
+            cy.contains('Logged in as').should('not.exist');
+        });
+    });
 });
