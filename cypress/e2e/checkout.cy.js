d358245f648da1e932a1eed8dd096f49db9aa5c4
expo25 <dsidari25@gmail.com>
2025-02-27 20:51:26 -0700

    fix: :bug: adjust locators

--- cypress/e2e/checkout.cy.js
+++ cypress/e2e/checkout.cy.js
@@ -19,14 +19,15 @@ describe('Checkout Process', () => {
             cy.contains('Shopping Cart').should('be.visible');
 
             // Proceed to checkout
-            cy.get('a[href="/login?checkout"]').contains('Proceed To Checkout').click();
+            cy.get('a.btn.check_out').contains('Proceed To Checkout').click();
 
             // Verify delivery address matches user data
-            cy.get('.address_delivery').within(() => {
+            cy.get('.checkout-information').within(() => {
                 cy.contains(this.userData.firstName + ' ' + this.userData.lastName).should('be.visible');
                 cy.contains(this.userData.address1).should('be.visible');
-                cy.contains(this.userData.city).should('be.visible');
+                cy.contains(this.userData.city + ' ' + this.userData.state + ' ' + this.userData.zipcode).should('be.visible');
             });
+            cy.get('a.btn.check_out').contains('Place Order').scrollIntoView().click();
 
             // This should be coming from a fixture file, I just did not have time to add it.
             cy.fillPaymentDetails({
@@ -41,7 +42,7 @@ describe('Checkout Process', () => {
             cy.get('button[data-qa="pay-button"]').click();
             // Verify success message then continue to homePage
             cy.contains('Order Placed!').should('be.visible');
-            cy.contains('Congratulations! Your order has been placed successfully!').should('be.visible');
+            cy.contains('Congratulations! Your order has been confirmed!').should('be.visible');
             cy.get('a[data-qa="continue-button"]').click();
             cy.url().should('eq', Cypress.config('baseUrl'));
         });
