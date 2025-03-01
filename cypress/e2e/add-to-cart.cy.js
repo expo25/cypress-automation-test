85fb4a39af0b82406f20e39d585baae18eb670bb
expo25 <dsidari25@gmail.com>
2025-02-28 18:54:37 -0700

    update comments: :bulb: add-to-cart test

--- cypress/e2e/add-to-cart.cy.js
+++ cypress/e2e/add-to-cart.cy.js
@@ -4,27 +4,27 @@ describe('Add an item to cart', () => {
     });
 
     it('Verify a user can search for a product and add it to the cart', () => {
-        // Navigate to Products page
+        // Navigate to Products page & perform product search
         cy.get('a[href="/products"]').click();
         cy.url().should('include', '/products');
         cy.contains('All Products').should('be.visible');
-        // Search for a product
         cy.get('input#search_product').type('Blue Top');
         cy.get('button#submit_search').click();
+
         // Verify search results
         cy.contains('Searched Products').should('be.visible');
         cy.get('.productinfo').contains('Blue Top').should('be.visible');
         cy.get('.productinfo').contains('Blue Top').parent().find('a[data-product-id]').click();
-        // Handle the "Added!" modal and continue
+
+        // Handle the "Added!" modal and continue to cart
         cy.get('.modal-content').contains('Added!').should('be.visible');
         cy.get('.modal-footer').contains('Continue Shopping').click();
-        // Navigate to Cart page
         cy.get('a[href="/view_cart"]').first().click();
         cy.url().should('include', '/view_cart');
-        // Validate the cart contains the correct item
+
+        // Validate the cart contains the correct item.
         cy.get('#product-1').within(() => {
             cy.contains('Blue Top').should('be.visible');
-            // I assume our quantity is '1' after this.
             cy.get('.cart_quantity').should('contain', '1');
         });
     });
